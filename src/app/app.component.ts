6f3b69ee9e9b97bbabd33022f9db8833952b0823
art <qwe522y@gmail.com>
2017-08-02 00:18:21 +0300

    top provider button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+<div>
+  <top-provider-but x="10" y="10"></top-provider-but>
+  <top-provider-but x="320" y="10"></top-provider-but>
+  <top-provider-but x="630" y="10"></top-provider-but>
+  <top-provider-but x="940" y="10"></top-provider-but>
+
+  <top-provider-but x="10" y="120"></top-provider-but>
+  <top-provider-but x="320" y="120"></top-provider-but>
+  <top-provider-but x="630" y="120"></top-provider-but>
+  <top-provider-but x="940" y="120"></top-provider-but>
+</div>
+  `
 })
 export class AppComponent {
   title = 'app';
+  
 }
